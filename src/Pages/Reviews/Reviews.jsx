51b97a76ff2d6d7a368dbd90f1fb66018f8896db
Maria <salyginamaria156@gmail.com>
2023-11-07 00:16:12 +0300

    added Reviews and Review

--- src/Pages/Reviews/Reviews.jsx
+++ src/Pages/Reviews/Reviews.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
-import Wrapper from '../../layouts/Wrapper/Wrapper'
+import React from "react";
+import Wrapper from "../../layouts/Wrapper/Wrapper";
+import s from "./Reviews.module.css";
+import Review from "./Review/Review";
 
+const reviews = [
+  {
+    id: 1,
+    name: "Даня",
+    review: "Хорошие специалисты. Сделали всё очень качественно."
+  },
+  {
+    id: 2,
+    name: "Анна",
+    review: "Круто получилось!"
+  },
+  {
+    id: 3,
+    name: "Витя",
+    review: "Всё супер. Всем советую."
+  },
+  {
+    id: 4,
+    name: "Никита",
+    review: `Я очень доволен ковриком, который я приобрёл от компании FastTafting. 
+      Он отлично сочетается с моим интерьером и создает уютную атмосферу в моем доме.`
+  },
+  {
+    id: 5,
+    name: "Костя",
+    review: "Это полный улёт!!!"
+  },
+]
 
 const Reviews = () => {
   return (
     <>
-      <Wrapper text="Отзывы"/>
+      <Wrapper text="Отзывы" />
+      <div className={s.reviews}>
+        <div className="container">
+          <div className={s.box}>
+            {
+              reviews.map(({id, name, review})=> (
+                <Review key={id} name={name} review={review}/>
+              ))
+            }
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Reviews
+export default Reviews;
